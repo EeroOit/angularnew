fd65dc08bc116af4868a6e5e2a3e5e0f6535a154
eerooit <eero.oittinen@gmail.com>
2021-02-11 14:52:16 +0200

    o

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloworldComponent } from './helloworld/helloworld.component';
 import {RouterModule} from '@angular/router';
 import {AppRouting} from './app-routing';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCardModule, MatSliderModule} from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FeedbackComponent } from './feedback/feedback.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RegistrationformComponent } from './registrationform/registrationform.component';
-import {MustMatchDirective} from './models/must-match.directive';
 import {CurrencyComponent} from './currency/currency.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -20,22 +16,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import {ReactiveRegistrationformComponent} from './reactive-registrationform/reactive-registrationform.component';
-import {QuestionsService} from './models/questions.service';
-import { CinemaComponent } from './cinema/cinema.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloworldComponent,
     CalculatorComponent,
-    FeedbackComponent,
-    RegistrationformComponent,
-    MustMatchDirective,
     CurrencyComponent,
-    MainNavComponent,
-    ReactiveRegistrationformComponent,
-    CinemaComponent
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
@@ -54,7 +42,6 @@ import { CinemaComponent } from './cinema/cinema.component';
     MatIconModule,
     MatListModule
   ],
-  providers: [ QuestionsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
