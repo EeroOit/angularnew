74358f46a5170f863e6bfa01a0e173fb7e4b58d6
eerooit <eero.oittinen@gmail.com>
2021-02-11 15:16:31 +0200

    swap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {RouterModule} from '@angular/router';
-import {AppRouting} from './app-routing';
-import { CalculatorComponent } from './calculator/calculator.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatSliderModule} from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import {CurrencyComponent} from './currency/currency.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CalculatorComponent,
-    CurrencyComponent,
-    MainNavComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule,
-    AppRouting,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatButtonModule,
-    FormsModule,
-    MatCardModule,
-    HttpClientModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    BrowserModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
